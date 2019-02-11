5178a413c9e023fc880cff6ea464b02e08f5b552
alexandru-dan.craciunescu@my.fmi.unibuc.ro <alxfmi589>
2019-02-11 22:08:08 +0200

    interface & import

---
+++ src/licenta/SceneController.java
@@ -0,0 +1,40 @@
+package licenta;
+
+import javafx.embed.swing.SwingFXUtils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+
+public class SceneController{
+
+    @FXML
+    private ImageView myImageView;
+    BufferedImage img;
+
+    //@Override
+    //public void initialize(URL location, ResourceBundle resources) {
+    //}
+
+    @FXML //for each fx:id=
+    private void loadimg(ActionEvent event) {
+        FileChooser file = new FileChooser();
+        file.setInitialDirectory(new File("D:\\personale\\fac\\lic"));
+        File file1 = file.showOpenDialog(null);
+        try {
+            img = ImageIO.read(file1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Image image  = SwingFXUtils.toFXImage(img,null); // ???
+        myImageView.setImage(image);
+    }
+
+    }
+
